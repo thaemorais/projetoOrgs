5c4b656ba1a036ee821a51a21b827e6ce5e516d4
Thaelen <109257207+thaemorais@users.noreply.github.com>
2024-04-27 08:13:04 -0300

    First Commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {SafeAreaView} from 'react-native';
+import {Home} from './src/telas/index.js';
+
+export function App() {
+  return (
+    <SafeAreaView>
+      <Home />
+    </SafeAreaView>
+  );
+}
+
+export default App;
